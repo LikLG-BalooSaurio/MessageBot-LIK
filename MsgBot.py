d391754e1f83258f55551b3dc759a9f400ccddf4
LikLG-BalooSaurio <96752603+LikLG-BalooSaurio@users.noreply.github.com>
2022-03-07 13:32:06 -0600

    Update MsgBot.py

--- MsgBot.py
+++ MsgBot.py
@@ -1,15 +1,20 @@
+from email.policy import default
+
 from dhooks import Webhook
+
 from colorama import Fore
+
 
 hook = Webhook("https://discord.com/api/webhooks/950213714311786536/bn32vlQE8NSlPiJowi1-q1xgASNFcRWY7l--uP1R3DUfhEGllEI2HlxBejmx3oCricc3")
 
-print(Fore.CYAN + """
 
+print(Fore.CYAN + """
 █████████████████████████████████████▀██████████████████████████
 █▄─▀█▀─▄█▄─▄▄─█─▄▄▄▄█─▄▄▄▄██▀▄─██─▄▄▄▄█▄─▄▄─███▄─▄─▀█─▄▄─█─▄─▄─█
 ██─█▄█─███─▄█▀█▄▄▄▄─█▄▄▄▄─██─▀─██─██▄─██─▄█▀████─▄─▀█─██─███─███
 ▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▀
 """)
+
 
 print(Fore.RED + """
                                                     ░█▀▀█ █──█ 　 ░█─── ▀█▀ ░█─▄▀ 
@@ -17,8 +22,11 @@ print(Fore.RED + """
                                                     ░█▄▄█ ▄▄▄█ 　 ░█▄▄█ ▄█▄ ░█─░█
 """)
 
-name = input(Fore.CYAN + "1. Introduce tu nombre: ")
 
-data = input(Fore.CYAN + "2. Introduce el mensaje: ")
 
-hook.send(name + "   `>>`   " + data)
+hook.username = input(Fore.CYAN + "1. Introduce tu nombre: ")
+
+
+hook.send(input(Fore.LIGHTCYAN_EX + "2. Introduce el mensaje: "))
+
+print(Fore.LIGHTWHITE_EX + "Listo! Mensaje enviado... Revisa el discord!")
