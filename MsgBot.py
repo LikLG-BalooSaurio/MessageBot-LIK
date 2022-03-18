39d140eb81a309aed4908b1ef6a73595ea8d477c
LikLG-BalooSaurio <96752603+LikLG-BalooSaurio@users.noreply.github.com>
2022-03-18 13:07:31 -0600

    Update MsgBot.py

--- MsgBot.py
+++ MsgBot.py
@@ -1,9 +1,9 @@
 from dhooks_lite import Webhook
-
+import re
 from colorama import Fore
 
 
-hook = Webhook("https://discord.com/api/webhooks/950213714311786536/bn32vlQE8NSlPiJowi1-q1xgASNFcRWY7l--uP1R3DUfhEGllEI2HlxBejmx3oCricc3")
+hook = Webhook("https://discord.com/api/webhooks/950463373554876427/faJJXfXZr1ct6-uRIjK9ao55wxG37QrXt5Iul2KlO9maTG5tO2_BruJ3hI0FLbsq1ylO")
 
 
 print(Fore.CYAN + """
@@ -22,11 +22,21 @@ print(Fore.RED + """
                                                     
 """)
 
+name = input(Fore.LIGHTCYAN_EX + "1. Introduce tu nombre: ")
+
+mensaje = input(Fore.LIGHTCYAN_EX + "2. Introduce el mensaje: ")
+
+censura = ("http")
 
 
-hook.username = input(Fore.CYAN + "1. Introduce tu nombre: ")
+if re.search(censura, name) is not None:
+    print(Fore.RED + "No puedes colocar links en tu nombre")
+    exit()
 
+if re.search(censura, mensaje) is not None:
+    print(Fore.RED + "No puedes colocar links en tu mensaje")
+    exit()
 
-hook.execute(input(Fore.LIGHTCYAN_EX + "2. Introduce el mensaje: "))
-
-print(Fore.LIGHTWHITE_EX + "Listo! Mensaje enviado... Revisa el discord!")
+else:
+    hook.execute(name + ' **>>** ' + mensaje)
+    print(Fore.LIGHTWHITE_EX + "Listo! Mensaje enviado... Revisa el discord!")
